45b236bca03c7e6fb0dc312d637dbf8979fc2ced
Patrick Downey <patrickdowney@dioadconsulting.com>
2021-01-26 22:35:38 +0000

    add ability to set flagset for viper config

--- config.go
+++ config.go
@@ -8,11 +8,14 @@ import (
 	"github.com/spf13/viper"
 )
 
-
 func InitViperConfig(orgName, appName string, cfg interface{}) error {
 	pflag.Parse()
+	return InitViperConfigWithFlagSet(orgName, appName, cfg, pflag.CommandLine)
+}
 
-	viper.BindPFlags(pflag.CommandLine)
+func InitViperConfigWithFlagSet(orgName, appName string, cfg interface{}, parsedFlagSet *pflag.FlagSet) error {
+
+	viper.BindPFlags(parsedFlagSet)
 
 	viper.SetConfigName(appName)
 	viper.SetConfigType("yaml")
