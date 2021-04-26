ff70e817cd67b8e308ae180af3660f9b2a148740
Patrick Downey <patrickdowney@dioadconsulting.com>
2021-04-26 22:31:34 +0100

    Add support for headless github authentication

--- auth/github.go
+++ auth/github.go
@@ -1,9 +1,39 @@
 package auth
 
 import (
+	"fmt"
+
 	"github.com/cli/oauth"
 	"github.com/cli/oauth/api"
 )
+
+func displayCode(userCode string, uri string) error {
+	fmt.Printf("Copy one-time code: %s\n", userCode)
+	return nil
+}
+
+func browseURL(uri string) error {
+	fmt.Println("Open %v in a browser and enter the code above when prompted.\n", uri)
+	return nil
+}
+
+func GitHubHeadlessDeviceLogin(clientID string, scopes []string) (*api.AccessToken, error) {
+	flow := &oauth.Flow{
+		Hostname: "github.com",
+		ClientID: clientID,
+		Scopes:   scopes,
+	}
+
+	flow.DisplayCode = displayCode
+	flow.BrowseURL = browseURL
+
+	accessToken, err := flow.DeviceFlow()
+	if err != nil {
+		return nil, err
+	}
+
+	return accessToken, nil
+}
 
 func GitHubDeviceLogin(clientID string, scopes []string) (*api.AccessToken, error) {
 	flow := &oauth.Flow{
