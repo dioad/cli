21c42a676043cd98723a0ebe544e33c1f380d968
Patrick Downey <patrickdowney@dioadconsulting.com>
2021-04-27 20:40:45 +0100

    Add utility wrappers around github device login

--- auth/github.go
+++ auth/github.go
@@ -7,6 +7,8 @@ import (
 	"github.com/cli/oauth/api"
 )
 
+type DeviceLoginFunc func(string, []string) (*api.AccessToken, error)
+
 func displayCode(userCode string, uri string) error {
 	fmt.Printf("One-time code: %s\n", userCode)
 	return nil
@@ -17,7 +19,7 @@ func browseURL(uri string) error {
 	return nil
 }
 
-func GitHubHeadlessDeviceLogin(clientID string, scopes []string) (*api.AccessToken, error) {
+func gitHubHeadlessDeviceLogin(clientID string, scopes []string) (*api.AccessToken, error) {
 	flow := &oauth.Flow{
 		Hostname: "github.com",
 		ClientID: clientID,
@@ -35,7 +37,7 @@ func GitHubHeadlessDeviceLogin(clientID string, scopes []string) (*api.AccessTok
 	return accessToken, nil
 }
 
-func GitHubDeviceLogin(clientID string, scopes []string) (*api.AccessToken, error) {
+func gitHubDeviceLogin(clientID string, scopes []string) (*api.AccessToken, error) {
 	flow := &oauth.Flow{
 		Hostname: "github.com",
 		ClientID: clientID,
@@ -48,4 +50,15 @@ func GitHubDeviceLogin(clientID string, scopes []string) (*api.AccessToken, erro
 	}
 
 	return accessToken, nil
+}
+
+func GitHubDeviceLogin(clientID string, scopes []string, headless bool) (*api.AccessToken, error) {
+	var deviceLoginFunc DeviceLoginFunc
+	if headless {
+		deviceLoginFunc = gitHubHeadlessDeviceLogin
+	} else {
+		deviceLoginFunc = gitHubDeviceLogin
+	}
+
+	return deviceLoginFunc(clientID, scopes)
 }
